b192f456c031da38298874b0a2c5014e225edb6a
Hugo Gomez Salas <hugogomezsalas@MacBook-Pro-de-Hugo-2.local>
2023-01-18 21:11:17 -0600

    habitaciones metodos

---
+++ apps/front/src/app/services/habitacion.ts
@@ -0,0 +1,69 @@
+import { AxiosError } from 'axios';
+import { servicesInstance } from '../utils/axios';
+import {
+  AddHabitacionParams,
+  AddHabitacionResponse,
+  getHabitacionesResponse,
+  Habitacion,
+  UpdateHabitacionParams,
+  UpdateHabitacionResponse,
+} from '@eagles/definitions';
+
+export const getHabitaciones = async (): Promise<Habitacion[]> => {
+  try {
+    const response = await servicesInstance.get<getHabitacionesResponse>(
+      '/habitacion'
+    );
+
+    return response.data.habitaciones;
+  } catch (error: unknown) {
+    console.error(error);
+    if (!(error as AxiosError).isAxiosError) return [];
+
+    return [];
+  }
+};
+
+export const deleteHabitacion = async (id: number): Promise<boolean> => {
+  try {
+    await servicesInstance.delete(`/habitacion/${id}`);
+    return true;
+  } catch (error: unknown) {
+    console.error(error);
+    if (!(error as AxiosError).isAxiosError) return false;
+    return false;
+  }
+};
+
+export const updateHabitacion = async (
+  id: number,
+  data: UpdateHabitacionParams
+): Promise<Habitacion> => {
+  try {
+    const response = await servicesInstance.put<UpdateHabitacionResponse>(
+      `/habitacion/${id}`,
+      data
+    );
+    return response.data.habitacion;
+  } catch (error: unknown) {
+    console.error(error);
+    if (!(error as AxiosError).isAxiosError) return {} as Habitacion;
+    return {} as Habitacion;
+  }
+};
+
+export const addHabitacion = async (
+  data: AddHabitacionParams
+): Promise<Habitacion> => {
+  try {
+    const response = await servicesInstance.post<AddHabitacionResponse>(
+      '/habitacion',
+      data
+    );
+    return response.data.habitacion;
+  } catch (error: unknown) {
+    console.error(error);
+    if (!(error as AxiosError).isAxiosError) return {} as Habitacion;
+    return {} as Habitacion;
+  }
+};
